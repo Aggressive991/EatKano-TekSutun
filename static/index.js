998079e4b574b89168bd98945fa482641b6b0cc4
Aggressive991 <aashjhjshj@outlook.com>
2026-09-11 19:36:14 +0300

    Refactor index handling in refreshGameLayer function
    Refactor refreshGameLayer function to reset index variable and improve clarity.

--- static/index.js
+++ static/index.js
@@ -194,6 +194,7 @@ const MODE_NORMAL = 1, MODE_ENDLESS = 2, MODE_PRACTICE = 3;
             id: "tap"
         });
         gameRestart();
+        refreshSize();
     }
 
     function gameRestart() {
@@ -297,7 +298,7 @@ const MODE_NORMAL = 1, MODE_ENDLESS = 2, MODE_PRACTICE = 3;
         _clearttClsReg = / t{1,2}\d+| bad/;
 
     function refreshGameLayer(box, loop, offset) {
-        let i = 0 + (loop ? 0 : 4);
+        let i = 0 ;
         for (let j = 0; j < box.children.length; j++) {
             let r = box.children[j], rstyle = r.style;
             rstyle.left = (j % 4) * blockSize + 'px';
@@ -312,7 +313,7 @@ const MODE_NORMAL = 1, MODE_ENDLESS = 2, MODE_PRACTICE = 3;
                 });
                 r.className += ' t' + (Math.floor(Math.random() * 1000) % 5 + 1);
                 r.notEmpty = true;
-                i = (Math.floor(j / 4) + 1) * 4 + Math.floor(Math.random() * 1000) % 4;
+                i = (Math.floor(j / 4) + 1) * 4 + 0 ;
             } else {
                 r.notEmpty = false;
             }
